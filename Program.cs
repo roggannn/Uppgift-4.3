86ad10036d5125f7f7f31b441ddb12b384f086c4
Pingvu <nhlpool27@gmail.com>
2023-11-30 13:15:19 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace uppgift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            for (int i = 0;i<=100;i++)
+            {
+
+            }
+
+
+
+
+
+            /*int i = 0;
+            while (i != 100)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                    i++;
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                    i++;
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                    i++;
+                }
+                else
+                Console.WriteLine(i);
+                i++*/
+               
+            }
+
+        }
+    }
+}
